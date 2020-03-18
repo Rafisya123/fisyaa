076b1f3d8df522c2ae671e526e54980c677e6a3c
Rafisya123 <60455061+Rafisya123@users.noreply.github.com>
2020-03-19 06:44:55 +0700

    Add files via upload

---
+++ tambah_aksi.php
@@ -0,0 +1,14 @@
+<?php
+	include 'koneksi.php';
+	
+	// menangkap data yang di kirim dari form
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$alamat = $_POST['alamat'];
+	
+	// menginput data ke database
+	mysqli_query($koneksi,"insert into mahasiswa values('$nim','$nama','$alamat')");
+	
+	// mengalihkan halaman kembali ke index.php
+	header("location:latihan1.php");
+?>
