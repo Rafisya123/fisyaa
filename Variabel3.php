73a2f3b32486f6b1f747668fd5f36ebb77cfedfa
Rafisya123 <60455061+Rafisya123@users.noreply.github.com>
2020-04-02 13:27:52 +0700

    Add files via upload

---
+++ Variabel3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Demo Variabel</title>
+</head>
+</body>
+
+<?php
+
+$bil = 8;
+var_dump($bil);
+// Output: int (8)
+
+$var = "";
+var_dump($var);
+// Output; string(0) ""
+
+$var = null;
+var_dump($var);
+// Output; NULL 
+?>
+
+</body>
+</html>
