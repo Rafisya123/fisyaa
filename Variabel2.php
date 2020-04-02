7df0ad728b48bc2eb86630835bc9870e068ad38c
Rafisya123 <60455061+Rafisya123@users.noreply.github.com>
2020-04-02 13:28:30 +0700

    Add files via upload

---
+++ Variabel2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Demo Variabel</title>
+</head>
+
+<?php 
+
+// Deklarasi dan inisialisasi 
+$bil = 8;
+
+// Dumping informasi mengenai variabel
+var_dump($bil);
+
+print_r($bil);
+
+?>
+
+</body>
+</html>
